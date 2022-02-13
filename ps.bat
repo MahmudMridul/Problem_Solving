234dfc01a4f46d5b29780add3e9abe2152e6fd0b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-02-13 11:23:35 +0600

    Batch script to start necessary stuffs

---
+++ ps.bat
@@ -0,0 +1,10 @@
+echo off
+cd/
+E:
+cd codes/cpp/problem_solving
+start .
+start https://codeforces.com/
+start https://docs.google.com/document/d/1m8rpBtdPGRLH8Fp7H6eYRMOF8X2cyyz8MtgUMg6Pu9A/edit
+start https://github.com/karansinghgit/CodeForces-Ladders
+start code .
+exit

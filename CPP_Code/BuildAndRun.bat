29fffa7371a83e3f72dc8a1c5be0bb38fd206b6c
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-09-14 23:18:14 +0600

    Batch script update

--- CPP_Code/BuildAndRun.bat
+++ CPP_Code/BuildAndRun.bat
@@ -2,8 +2,10 @@
 echo Build started
 g++ main.cpp -o main && ( 
     echo Build completed
-    echo running...
+    echo Running...
     main.exe
+    for /f "tokens=* delims=" %%x in (output.txt) do echo %%x
+    echo Done
 ) || (
     echo Build failed
 )

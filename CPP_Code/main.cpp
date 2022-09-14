355928b8ed9bffba21db2d84fe5fb20929d5cd62
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-09-14 23:43:16 +0600

    Time stuff

--- CPP_Code/main.cpp
+++ CPP_Code/main.cpp
@@ -1,6 +1,7 @@
 //#pragma GCC optimize("Ofast")
 
 #include <bits/stdc++.h>
+#include <chrono>
 
 #define fast ios_base::sync_with_stdio(false)
 #define clr cin.tie(NULL)
@@ -28,6 +29,7 @@
 #define pi 3.1415926535897932384626433832795
 
 using namespace std;
+using namespace std::chrono;
 
 template<typename type>
 void readArray(type arr[], int size)
@@ -38,7 +40,13 @@ void readArray(type arr[], int size)
 template<typename type>
 void readVector(vector<type>& vec, int size)
 {
-    for(int i=0; i<size; ++i) { int x; i1(x); vec.push_back(x); }
+    for(int i=0; i<size; ++i) { type x; i1(x); vec.push_back(x); }
+}
+
+template<typename type>
+void readSet(set<type>& list, int size)
+{
+    for(int i=0; i<size; ++i) { type x; i1(x); list.insert(x); }
 }
 
 template<typename type>
@@ -60,10 +68,61 @@ void printVectorPair(vector< pair<type, type> >& vec, int size)
 }
 
 
-void solve(string p1[], string p2[], string p3[], int numofWords) 
+void solve(set<string> p1, set<string> p2, set<string> p3, int numofWords) 
 {
-   printArray(p1, numofWords);
+    int p1Points = 0;
+    int p2Points = 0;
+    int p3Points = 0;
+
+    set<string>::iterator iter1 = p1.begin();
+    set<string>::iterator iter2 = p2.begin();
+    set<string>::iterator iter3 = p3.begin();
+
+    while(iter1 != p1.end())
+    {
+        string word1 = *(iter1);
+        string word2 = *(iter2);
+        string word3 = *(iter3);
+
+        //For three points
+        if(!p2.count(word1) && p3.count(word1) != 1)
+        {
+            p1Points += 3;
+        }
+        if(!p1.count(word2) && !p3.count(word2))
+        {
+            p2Points += 3;
+        }
+        if(!p1.count(word3) && !p2.count(word3))
+        {
+            p3Points += 3;
+        }
+
+        //For one point
+        if((!p2.count(word1) && p3.count(word1)) || (p2.count(word1) && !p3.count(word1)))
+        {
+            p1Points += 1;
+        }
+        if((!p1.count(word2) && p3.count(word2)) || (p1.count(word2) && !p3.count(word2)))
+        {
+            p2Points += 1;
+        }
+        if((!p1.count(word3) && p2.count(word3)) || (p1.count(word3) && !p2.count(word3)))
+        {
+            p3Points += 1;
+        }
+
+
+        ++iter1;
+        ++iter2;
+        ++iter3;
+    }
+
+    os(p1Points);
+    os(p2Points);
+    ol(p3Points);
 }
+
 
 
 int main()
@@ -76,19 +135,31 @@ int main()
     freopen("output.txt", "w", stdout);
     #endif
 
+    #ifndef ONLINE_JUDGE
+    //clock_t startOverall = clock();
+    auto start = high_resolution_clock::now();
+    #endif
+
     lli test, numberofWords;
     i1(test);
 
     while(test--)
     {
         i1(numberofWords);
-        string p1[numberofWords]; string p2[numberofWords]; string p3[numberofWords];
-        readArray(p1, numberofWords);
-        readArray(p2, numberofWords);
-        readArray(p3, numberofWords);
+        set<string> p1; set<string> p2; set<string> p3;
+        readSet(p1, numberofWords);
+        readSet(p2, numberofWords);
+        readSet(p3, numberofWords);
         solve(p1, p2, p3, numberofWords);
     }
-    
+
+    #ifndef ONLINE_JUDGE
+    //clock_t endOverall = clock();
+    auto end = high_resolution_clock::now();
+    //double durationOverall = double(endOverall-startOverall) / double(CLOCKS_PER_SEC); 
+    auto duration = duration_cast<milliseconds>(end - start);
+    printf("%.10f\n",duration);
+    #endif
     return 0;
 }
 

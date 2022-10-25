d8d0af2dafab330305ddbff7e549c08c93e74185
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-10-25 08:59:52 +0600

    3 new functions

--- Topics/BinarySearch.cpp
+++ Topics/BinarySearch.cpp
@@ -25,6 +25,68 @@ int smallest_greater_equal(vector<int>& list, int size, int num)
     return low;
 }
 
+int smallest_greater(vector<int>& list, int size, int num)
+{
+    int low = 0, high = size - 1;
+
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(list[mid] > num)
+        {
+            high = mid - 1;
+        }
+        else 
+        {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
+
+int largest_less(vector<int>& list, int size, int num)
+{
+    int low = 0, high = size - 1;
+
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(list[mid] < num)
+        {
+            low = mid + 1;
+        }
+        else 
+        {
+            high = mid - 1;
+        }
+    }
+
+    return high;
+}
+
+int largest_less_equal(vector<int>& list, int size, int num)
+{
+    int low = 0, high = size - 1;
+
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(list[mid] <= num)
+        {
+            low = mid + 1;
+        }
+        else 
+        {
+            high = mid - 1;
+        }
+    }
+
+    return high;
+}
 
 int main()
 {

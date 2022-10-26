05728774c66c8c3bac4612bb6aaa6924c2fa952f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-10-26 18:30:44 +0600

    tokenize() added

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 
 #include <bits/stdc++.h>
 
-#define t_st clock_t start = clock();
-#define t_en clock_t stop = clock();
-#define t_p double duration = double(stop-start) / double(CLOCKS_PER_SEC); printf("RUNTIME: %.10f\n",duration);
-
 #define endline "\n"
 
 #define ulli unsigned long long int //64
@@ -54,73 +50,41 @@ void printVector(vector<type>& vec, int n)
     }
 }
 
+
 template<typename type>
 void printVectorPair(vector< pair<type, type> >& vec, int size)
 {
     for(int i=0; i<size; ++i) { cout << vec[i].first << " " << vec[i].second << endline; }
 }
 
-// SOLVE : https://codeforces.com/contest/1742/problem/F
-
-void solve(vector<int>& arr, int n)
+void tokenize(string str, char delim)
 {
-    vector<int> pre;
+    stringstream ss(str);
+    string word;
 
-    sort(arr.begin(), arr.end(), greater<int>());
-    pre.push_back(arr[0]);
-    int val = arr[0];
-    
-    arr[0] = -1;
-
-    int upto = ceil(log2(n));
-
-    for(int c = 1; c <= min(upto, n - 1); ++c)
+    while(!ss.eof())
     {
-        int max_val = -1;
-        int max_val_in = 0;
-        for(int i = n - 1; i >= 0; --i)
-        {
-            if(arr[i] != -1)
-            {
-                int temp = val | arr[i];
-                if(temp >= max_val)
-                {
-                    max_val = val | arr[i];
-                    max_val_in = i;
-                }
-            }
-        }
-        val = val | arr[max_val_in];
-        pre.push_back(arr[max_val_in]);
-        arr[max_val_in] = -1;
-
+        getline(ss, word, delim);
+        cout << word << endline;
     }
-
-    for(int i = 0; i < n; ++i)
-    {
-        if(arr[i] != -1)
-        {
-            pre.push_back(arr[i]);
-        }
-    }
-
-    printVector(pre, pre.size());
-
-
 }
 
+
+void solve(vector<char>& stamp, int size)
+{
+    tokenize("RBWBWRRBW", 'W');
+}
 
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(NULL);
 
     #ifndef ONLINE_JUDGE
+    clock_t start = clock();
     freopen("TextFiles/input.txt", "r", stdin);
     freopen("TextFiles/output.txt", "w", stdout);
-    t_st
     #endif
 
     int test, n;
@@ -129,14 +93,15 @@ int main()
     while(test--)
     {
         cin >> n;
-        vector<int> arr;
-        readVector(arr, n);
-        solve(arr, n);
+        vector<char> stamp;
+        readVector(stamp, n);
+        solve(stamp, n);
     }
 
     #ifndef ONLINE_JUDGE
-    t_en
-    t_p
+    clock_t stop = clock();
+    double duration = double(stop-start) / double(CLOCKS_PER_SEC); 
+    printf("RUNTIME: %.10f\n",duration);
     #endif
 
 
